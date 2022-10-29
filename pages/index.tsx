c34766ab3de48049bd70912d2b2ac1207a4bd9ca
Henry <henrykc24@gmail.com>
2022-10-29 19:42:55 +0100

    Add home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} blue-gradient`}>
       <Head>
         <title>Custxmer App</title>
         <meta
@@ -14,15 +15,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Custxmer!</h1>
+        <div className={styles.innerContainer}>
+          <div className={styles.logoContainer}>
+            <Image
+              width={400}
+              height={100}
+              className={styles.logo}
+              src="/logo_slogan.png"
+              alt="Logo"
+            />
+          </div>
+          <div className={styles.loginSignup}>
+            <button
+              className={`second-blue ${styles.button} white white-shadow`}
+            >
+              SIGN UP
+            </button>
+            <button
+              className={`main-blue ${styles.button} main-blue-shadow`}
+            >
+              LOG IN
+            </button>
+          </div>
+        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <p>
-          <span>{new Date().getFullYear()}</span> by Custxmer Inc. | Terms &
-          Conditions - Privacy Policy
-        </p>
-      </footer>
     </div>
   );
 }
