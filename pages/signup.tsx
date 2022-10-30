548a632be1aa32d663fe10d6fdc2a6af3f7f2a0b
Henry <henrykc24@gmail.com>
2022-10-30 20:59:46 +0100

    Add sign up component

---
+++ pages/signup.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+const signup = () => {
+  return (
+    <main>
+      <div>
+        <h1>Sign Up</h1>
+
+      </div>
+    </main>
+  );
+};
+
+export default signup;
