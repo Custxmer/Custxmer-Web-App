eb9bf81e0fc20d0e1e1aff4089bea0ce9256cfff
Henry <henrykc24@gmail.com>
2022-11-01 00:06:50 +0100

    Complete Sign Up UI

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -15,11 +15,25 @@ const Signup = () => {
           alt="Logo"
         />
         <h2>First Thing First!</h2>
-        <p>Currently we are operating in:<br />- Ireland, and<br />- The US</p>
-        <button className={`button main-blue ${styles.okay}`}>THAT'S OKAY</button>
+        <p>
+          Currently we are operating in:
+          <br />- Ireland, and
+          <br />- The US
+        </p>
+        <button className={`button main-blue ${styles.okay}`}>
+          THAT'S OKAY
+        </button>
         <div className={styles.noCountry}>
-          <div className={styles.innerDiv}>kk</div>
+          <div className={styles.innerDiv}>
+            <h4 className={styles.letYouKnow}>DON'T SEE YOUR CONTRY?</h4>
+            <p>
+              Tap here to let us know.
+              <br />
+              We will let you know when we add it.
+            </p>
+          </div>
         </div>
+        <button className={`button ${styles.already}`}>ALREADY HAVE AN ACCOUNT?</button>
       </div>
     </main>
   );
