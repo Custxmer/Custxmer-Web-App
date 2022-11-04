826708ef307399d0696b18525376d41f60e11ab9
Henry <henrykc24@gmail.com>
2022-11-04 12:55:04 +0100

    Add responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,30 +13,33 @@ export default function Home() {
           content="A web application that helps you contact your service providers with ease."
         />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css"></link>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.12.0/css/all.css"
+        ></link>
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.innerContainer}>
-          <div className={styles.logoContainer}>
-            <Image
-              width={400}
-              height={100}
-              className={styles.logo}
-              src="/logo_slogan.png"
-              alt="Logo"
-            />
-          </div>
-          <div className={styles.loginSignup}>
-            <Link href="/signup">
-              <button className={`second-blue button white white-shadow ${styles.signup}`}>
-                SIGN UP
-              </button>
-            </Link>
-            <button className={`main-blue button main-blue-shadow`}>
-              LOG IN
+      <main className={styles.innerContainer}>
+        <div className={styles.logoContainer}>
+          <Image
+            width={400}
+            height={100}
+            className={styles.logo}
+            src="/logo_slogan.png"
+            alt="Logo"
+          />
+        </div>
+        <div className={styles.loginSignup}>
+          <Link href="/signup">
+            <button
+              className={`second-blue button white white-shadow ${styles.signup}`}
+            >
+              SIGN UP
             </button>
-          </div>
+          </Link>
+          <button className={`main-blue button main-blue-shadow`}>
+            LOG IN
+          </button>
         </div>
       </main>
     </div>
