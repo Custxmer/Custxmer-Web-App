d2047bd20e5f4fbc94cac6480b65f74613864e49
Henry <henrykc24@gmail.com>
2022-11-09 16:10:56 +0100

    Fetch countries from an API

--- pages/proposeACountry.tsx
+++ pages/proposeACountry.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/ProposeACountry.module.css';
 
-import countryList from '../utils/countries';
+// import countryList from '../utils/countries';
 
 const proposeACountry = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const data = await fetch(
+        'https://api.adalo.com/v0/apps/d555f7ca-f23c-47bc-a0e6-8caf0e046da8/collections/t_ab3581141de44cd19ef7cf36462a3da7?limit=200',
+        {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer eo14jtmxpm3pyheppkmuzkb47',
+          },
+        }
+      );
+      const countries = await data.json();
+      console.log('countries', countries);
+      const countriesArray = countries.records.map(
+        (country: {
+          id: number;
+          'Country name': string;
+          created_at: string;
+          updated_at: string;
+        }) => country['Country name']
+      );
+      console.log('countries array', countriesArray);
+      setCountries(countriesArray);
+    };
+    fetchCountries();
+  }, []);
   return (
     <div className={styles.parent}>
       <form className={styles.form}>
-        <h1 className={styles.heading}>Propose a Country</h1>
+        <h1 className={`${styles.heading} main-blue`}>Propose a Country</h1>
         <p>
           Your email address.
           <br />
           So we can let you know once we add it.
         </p>
-        <input type="email" placeholder="Enter your email address..." />
+        <input
+          required
+          type="email"
+          placeholder="Enter your email address..."
+        />
         <label htmlFor="countries">
           <div>Country</div>
           <select
@@ -21,10 +54,11 @@ const proposeACountry = () => {
             id="countries"
             placeholder="Select a country"
           >
-            <option>Select a Country...</option>
-            {countryList.map((country) => (
-              <option key={country}>{country}</option>
-            ))}
+            <option className={styles.placeholder}>Select a Country...</option>
+            {countries[0] &&
+              countries.map((country) => (
+                <option key={country}>{country}</option>
+              ))}
           </select>
         </label>
         <p>
@@ -39,7 +73,12 @@ const proposeACountry = () => {
           rows={3}
           placeholder="Names, info and the service(s) they provide..."
         ></textarea>
-        <input type="submit" value="SUBMIT COUNTRY SUGGESTION" />
+        <div className={styles.buttonsContainer}>
+          <button className={`${styles.cancel} main-blue`}>Cancel</button>
+          <button className={styles.submit} type="submit">
+            SUBMIT
+          </button>
+        </div>
       </form>
     </div>
   );
