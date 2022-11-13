093a6fc33bbcedaf9c375c76ef03a3f093d574e8
Henry <henrykc24@gmail.com>
2022-11-13 20:45:32 +0100

    Add proposalSent modal

---
+++ components/modals/ProposalSent.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from 'react';
+import styles from '../../styles/components/ProposalSent.module.css';
+import { useRouter } from 'next/router';
+import lottie from 'lottie-web';
+
+interface Props {
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ProposalSent = ({ setShowModal }: Props) => {
+  const router = useRouter();
+  const emailContainer = useRef(null);
+  const animationContainer = useRef(null);
+
+  useEffect(() => {
+    const instance = lottie.loadAnimation({
+      container: animationContainer.current,
+      render: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../../utils/balancingFlag.json'),
+    });
+    return () => instance.destroy();
+  }, []);
+  return (
+    <div
+      onClick={() => {
+        setShowModal(false);
+        router.push('/');
+      }}
+      className="modal"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`${styles.innerDiv} second-blue`}
+      >
+        <span>
+          <strong
+            onClick={() => {
+              setShowModal(false);
+              router.push('/');
+            }}
+          >
+            &times;
+          </strong>
+        </span>
+        <div className={styles.animationContainer}>
+          <div className={styles.animation} ref={animationContainer}></div>
+        </div>
+        <h4>Proposal Sent!</h4>
+        <p>
+          Thanks for proposing.
+          <br />
+          The more people ask for it, the sooner we will add it.
+          <br />
+          We'll let you know when we do.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProposalSent;
