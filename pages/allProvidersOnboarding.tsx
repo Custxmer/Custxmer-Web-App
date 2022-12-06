8045c5a56e2b95cc9819a65d3f2052f93fda27ad
Henry <henrykc24@gmail.com>
2022-12-06 22:26:19 +0100

    Add more components

--- pages/allProvidersOnboarding.tsx
+++ pages/allProvidersOnboarding.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const allProvidersOnboarding = () => {
   return (
-    <div>allProvidersOnboarding</div>
-  )
-}
+    <section>
+      <section>
+        <div>
+          <span>search</span>
+          <input
+            type="text"
+            placeholder="Search providers or select them below..."
+          />
+          <span>close</span>
+        </div>
+      </section>
+      <aside>
+        <h2>All Providers</h2>
+        <p>25 providers in Ireland</p>
+        <p>
+          Let's start by adding one to three providers in your list. You can add
+          more later
+        </p>
+        <p>If you can't find one, please request it below.</p>
+      </aside>
+      <aside>
+        <h2>Click a provider to view details here.</h2>
+      </aside>
+    </section>
+  );
+};
 
-export default allProvidersOnboarding
+export default allProvidersOnboarding;
